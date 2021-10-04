339ff628e8153cc7ed33f9686ce9b6cfe87a4a04
unknown <dunvph10007@fpt.edu.vn>
2021-10-04 12:57:01 +0700

    fix part1

--- js/script.js
+++ js/script.js
@@ -10,25 +10,20 @@ $(document).ready(function () {
     });
     $('.search-icon').click(function (e) { 
         e.preventDefault();
-        //$('.search-input').toggle(500);
         $(this).css('color', '#dd2f31');
-        //$(this).classList.toggle("menu-icon-active");
     });
-    $('.col-inner').mouseover(function () { 
+    $('.top-selling .col-inner').mouseover(function () { 
         $(this).children('.product-icon').css({
             'display':'block',
-
         });
         $(this).css({
             "box-shadow": "rgba(67, 71, 85, 0.27) 0px 0px 0.25em, rgba(90, 125, 188, 0.05) 0px 0.25em 1em",
-            "border": "1px solid #f0ecec"
         })
     });
-    $('.col-inner').mouseout(function () { 
+    $('.top-selling .col-inner').mouseout(function () { 
         $(this).children('.product-icon').css('display', 'none');
         $(this).css({
             "box-shadow": "none",
-            "border": "none"
         })
     });
 });
